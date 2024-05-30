57f7618d9fc70d6c5ca60ae15be43f8eca96e0c2
Diana Pilar Miranda Estrella <miranda_uxz@hotmail.com>
2024-05-29 18:57:21 -0600

    studentdashboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/LoginPage';
+import StudentDashboard from './pages/StudentDashboard';
 
 function App() {
   return (
@@ -9,6 +10,14 @@ function App() {
       <BrowserRouter>
           <Routes>
               <Route path="/login" element={<Login/>} />
+              <Route path="/alumnos-caafi" element={<StudentDashboard/>} />
+              <Route path="/administador-caafi" element={<AdminDashboard/>} />
+              <Route path="/cabina-caafi" element={<CabinDashboard/>} />
+              <Route path="/registro-alumnos" element={<StudentRegister/>} />
+              <Route path="/registro-personal" element={<PersonalRegister/>} />
+              <Route path="/solicitudes-alumnos" element={<StudentsApplications/>} />
+              <Route path="/bitacora" element={<Binnacle/>} />
+              <Route path="/reportes" element={<Report/>} />
               <Route path="*" element={<h1>404</h1>} />
           </Routes>
       </BrowserRouter>
