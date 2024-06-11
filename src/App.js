f9435c50e0a5e1e9895ca0880bd2014462fe81df
mirandasm3 <130334715+mirandasm3@users.noreply.github.com>
2024-06-11 00:36:27 -0600

    pagessss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/LoginPage';
-import StudentDashboard from './pages/StudentDashboard';
 import SingUpPersonal from './pages/SingUpPersonal';
+import SelectStudent from './pages/SelectStudent';
+import Dashboard from './pages/Dashboard';
+import BinnacleHistory from './pages/BinnacleHistory';
+import StatsReports from './pages/StatsReports';
+import RegistrationRequest from './pages/RegistrationRequest';
 
 
 function App() {
@@ -12,8 +16,12 @@ function App() {
           <Routes>
               <Route path="/" element={<Navigate to="/caafi" />} />
               <Route path="/caafi" element={<Login/>} />
-              <Route path="/alumnos-caafi" element={<StudentDashboard/>} />
+              <Route path="/registro" element={<SelectStudent/>} />
+              <Route path="/solicitud-registro" element={<RegistrationRequest/>} />
+              <Route path="/inicio" element={<Dashboard/>} />
               <Route path="/registrar-personal-caafi" element={<SingUpPersonal/>} />
+              <Route path="/reportes" element={<StatsReports/>} />
+              <Route path="/historial-bitacoras" element={<BinnacleHistory/>} />
               <Route path="*" element={<h1>404</h1>} />
           </Routes>
       </BrowserRouter>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,36 +1,41 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie';
-import "../styles/login.css"
+import { Modal, Button } from 'react-bootstrap';
+import "../styles/login.css";
 
 export default function Login() {
-    const [user, setUser] = useState("")
-    const [password, setPassword] = useState("")
-    const [formError, setFormError] = useState("")
-    const [passwordVisible, setPasswordVisible] = useState(false)
-    const [userInputIsInvalid, setUserInputIsInvalid] = useState(false)
-    const [passwordInputIsInvalid, setPasswordInputIsInvalid] = useState(false)
-    
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+    const [formUserError, setFormUError] = useState("");
+    const [formPassError, setFormPError] = useState("");
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [userInputIsInvalid, setUserInputIsInvalid] = useState(false);
+    const [passwordInputIsInvalid, setPasswordInputIsInvalid] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+
     const navigate = useNavigate();
 
-    const logoUrl = require('../img/caafi.png')
+    const logoUrl = require('../img/caafi.png');
+    const uv = require('../img/Universidad-Veracruzana-Title.png');
 
     const checkFormFields = () => {
-        setFormError("");
+        setFormUError("");
+        setFormPError("");
         setUserInputIsInvalid(false);
         setPasswordInputIsInvalid(false);
         if(user === "") {
-            setFormError("Usuario requerido");
+            setFormUError("Usuario requerido");
             setUserInputIsInvalid(true);
             return;
         }
         if (!/^(s|S)\d{8}$|^[a-z]+$/.test(user)) {
-            setFormError("Usuario inválido");
+            setFormUError("Usuario inválido");
             setUserInputIsInvalid(true);
             return;
         }        
         if(password === "") {
-            setFormError("Contraseña requerida");
+            setFormPError("Contraseña requerida");
             setPasswordInputIsInvalid(true);
             return;
         }
@@ -38,7 +43,7 @@ export default function Login() {
     }
 
     const logIn = () => {
-        fetch(`${"localhost:9000"}/auth/login`, {
+        fetch(`${"http://localhost:9000"}/auth/login`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
@@ -57,58 +62,103 @@ export default function Login() {
                     break;
                 }
                 case 401: {
-                    setFormError("Credenciales inválidas");
+                    setFormPError("Credenciales inválidas");
                     break;
                 }
                 case 429: {
-                    setFormError("Demasiados intentos");
+                    setFormPError("Demasiados intentos");
                     break;
                 }
                 default: {
-                    setFormError("Error desconocido");
+                    setFormPError("Error desconocido");
                     break;
                 }
             }
         })
     }
 
-    return <div className="d-flex align-items-center justify-content-center vh-100">
-        <form>
-            <div className="cardo mx-auto login-form">
-                <div className="cardo-body">
-                    <div className="logo">
-                        <img src={logoUrl} alt="Logo CAAFI"/>
-                        <h4>Inicio de sesión</h4>
-                    </div>
-                    <div className="mb-3">
-                        <label for="input-user" className="form-label">{"Usuario"}</label>
-                        <input type="user" className={!userInputIsInvalid ? "form-control" : "form-control is-invalid"} id="input-user" value={user} onChange={ev => setUser(ev.target.value)} placeholder="Matrícula o cuenta" autoComplete="off"/>
-                        <span className="error-message text-danger card-text">{formError}</span>
-                    </div>
-                    <div className="mb-3">
-                        <label for="input-password" className="form-label">{"Contraseña"}</label>
-                        <div className="input-group">
-                            <input type={passwordVisible ? "text" : "password"} className={!passwordInputIsInvalid ? "form-control" : "form-control is-invalid"} id="input-password" value={password} onChange={ev => setPassword(ev.target.value)} autoComplete="off"/>
-                            <span className="input-group-text" onClick={() => setPasswordVisible(!passwordVisible)} style={{cursor: "pointer"}}>
-                                <i className={!passwordVisible ? "bi bi-eye" : "bi bi-eye-slash"} id="togglePassword"></i>
-                            </span>
+    return (
+        <div className="login-background">
+            <div className="d-flex align-items-center justify-content-center vh-100">
+                <form autoComplete="off">
+                    <div className="cardo mx-auto login-form">
+                        <div className="cardo-body">
+                            <div className="m-login__head" style={{ position: 'absolute', top: '0', left: '0', backgroundColor: '#0D47A1', zIndex: '1000', padding: '10px' }}>
+                                <span className="g-font-weight-400 g-py-0 g-px-12 g-font-size-18" style={{ color: '#ffffff' }}>Universidad Veracruzana</span>
+                            </div>
+                            <div className="logo">
+                                <img src={logoUrl} alt="Logo CAAFI" />
+                                <h4>Inicio de sesión</h4>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="input-user" className="form-label">{"Usuario"}</label>
+                                <input 
+                                    type="text" 
+                                    className={!userInputIsInvalid ? "form-control" : "form-control is-invalid"} 
+                                    id="input-user" 
+                                    name="noautocomplete-user"
+                                    value={user} 
+                                    onChange={ev => setUser(ev.target.value)} 
+                                    placeholder="Matrícula o cuenta" 
+                                    autoComplete="off"
+                                />
+                                <span className="error-message text-danger card-text">{formUserError}</span>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="input-password" className="form-label">{"Contraseña"}</label>
+                                <div className="input-group">
+                                    <input 
+                                        type={passwordVisible ? "text" : "password"} 
+                                        className={!passwordInputIsInvalid ? "form-control" : "form-control is-invalid"} 
+                                        id="input-password" 
+                                        name="noautocomplete-password"
+                                        value={password} 
+                                        onChange={ev => setPassword(ev.target.value)} 
+                                        autoComplete="off"
+                                    />
+                                    <span className="input-group-text" onClick={() => setPasswordVisible(!passwordVisible)} style={{cursor: "pointer"}}>
+                                        <i className={!passwordVisible ? "bi bi-eye" : "bi bi-eye-slash"} id="togglePassword"></i>
+                                    </span>
+                                </div>
+                                <span className="error-message text-danger card-text">{formPassError}</span>
+                            </div>
+                            <div className="d-flex flex-row-reverse align-items-center justify-content-center">
+                                <button 
+                                    type="button" 
+                                    className="btn btn-success" 
+                                    style={{ width: "100%", maxWidth: "calc(100% - 5px)" }} 
+                                    onClick={checkFormFields}
+                                >
+                                    {"Iniciar sesión"}
+                                </button>
+                            </div>
+                            <p className="align-items-center justify-content-center">o</p>
+                            <div className="text-end">
+                                <a className="ar-text" onClick={() => navigate("/registro")}>{"Solicita tu inscripción AQUÍ"}</a>
+                                <br/><br/>
+                                <a className="rp-text" href="#" onClick={(e) => {e.preventDefault();setShowModal(true);}}>{"¿Olvidaste tu contraseña?"}</a>
+                            </div>
                         </div>
                     </div>
-                    <div className="d-flex flex-row-reverse align-items-center justify-content-center">
-                        <button type="button" className="btn btn-success" style={{ width: "100%", maxWidth: "calc(100% - 5px)" }} onClick={checkFormFields}>{"Iniciar sesión"}</button>
-                    </div>
-
-                    <p className="align-items-center justify-content-center">o</p>
-                    <div className="text-end w-100 align-items-center justify-content-center">
-                        <a className="ar-text" onClick={() => navigate("/solicitar-registo")}>{"Solicita tu inscripción AQUÍ"}</a>
-                        <br></br>
-                        <a className="rp-text" onClick={() => navigate("/recuperar-cuenta")}>{"¿Olvidaste tu contraseña?"}</a>
-                    </div>
+                </form>
+                <div className="footer-text">
+                    © 2024 Universidad Veracruzana. Todos los derechos reservados
                 </div>
+
+                <Modal show={showModal} onHide={() => setShowModal(false)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Recuperar contraseña</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Para realizar un cambio de contraseña, visita CAAFI y solicita ayuda al técnico de cabina en turno.
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => setShowModal(false)}>
+                            Cerrar
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
-        </form>
-        <div className="footer-text">
-                © 2024 Universidad Veracruzana. Todos los derechos reservados
         </div>
-    </div>
+    );
 }
