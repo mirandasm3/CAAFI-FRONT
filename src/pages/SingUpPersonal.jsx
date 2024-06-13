14299fb106733424be62bd1e6461b7ace4124dab
Raudel Morales <94869042+prince-edu@users.noreply.github.com>
2024-06-13 14:26:37 -0600

    segun terminada
    ...

--- src/pages/SingUpPersonal.jsx
+++ src/pages/SingUpPersonal.jsx
@@ -25,83 +25,78 @@ export default function SingUpPersonal() {
     };
 
     return (
-            <div className="d-flex align--center justify-content- vh-100" style={{ flexDirection: 'column' }}>
+        <div className="d-flex align--center justify-content- vh-100" style={{ flexDirection: 'column' }}>
+            
+            <div className="grey-block">
+                <img src={logoURL} id="caafi-logo" alt="logo" />
 
-                <div className="grey-block">
-                        <img src={logoURL} id="caafi-logo" alt="logo" />
-
-                        <div className="column-container">
-                            <img src={uvTitleURL} id="uv-title" alt="universidad veracruzana" />
-                            <img src={userIconURL} id="userIconImage" alt="icon" />
-                        </div>
-
+                    <div className="column-container">
+                        <img src={uvTitleURL} id="uv-title" alt="universidad veracruzana" />
+                        <img src={userIconURL} id="userIconImage" alt="icon" />
                     </div>
+            </div>
 
-                    
-                    <h1 className="singup-title">
-                        Registro de Personal CAAFI
-                    </h1>
-                
-            <div className="signup-form">
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                <label htmlFor="usuario">Usuario:</label>
-                <input
-                    type="text"
-                    id="usuario"
-                    value={usuario}
-                    onChange={(e) => setUsuario(e.target.value)}
-                    required
-                />
-                </div>
-                <div className="form-group">
-                <label htmlFor="apellidos">Apellidos:</label>
-                <input
-                    type="text"
-                    id="apellidos"
-                    value={apellidos}
-                    onChange={(e) => setApellidos(e.target.value)}
-                    required
-                />
-                </div>
-                <div className="form-group">
-                <label htmlFor="contrasena">Contraseña:</label>
-                <input
-                    type="password"
-                    id="contrasena"
-                    value={contrasena}
-                    onChange={(e) => setContrasena(e.target.value)}
-                    required
-                />
-                </div>
-                <div className="form-group">
-                <label htmlFor="nombre">Nombre(s):</label>
-                <input
-                    type="text"
-                    id="nombre"
-                    value={nombre}
-                    onChange={(e) => setNombre(e.target.value)}
-                    required
-                />
-                </div>
-                <div className="form-group">
-                <label htmlFor="tipoUsuario">Puesto:</label>
-                <select
-                    id="tipoUsuario"
-                    value={tipoUsuario}
-                    onChange={(e) => setTipoUsuario(e.target.value)}
-                    required>
-                    //opciones solo de ejemplo para probar
-                    <option value="">Seleccionar tipo</option>
-                    <option value="estudiante">Estudiante</option>
-                    <option value="profesor">Profesor</option>
-                    <option value="administrativo">Administrativo</option>
-                </select>
-                </div>
-                <button type="submit" className="submit-button">Registrar</button>
-            </form>
+                <h1 className="singup-title">Registro de Personal CAAFI</h1>
+                <div className="signup-form">
+                    <form onSubmit={handleSubmit}>
+                       <div className="form-group">
+                          <label htmlFor="usuario">Usuario:</label>
+                          <input
+                            type="text"
+                            id="usuario"
+                            value={usuario}
+                            onChange={(e) => setUsuario(e.target.value)}
+                            required
+                             />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="apellidos">Apellidos:</label>
+                            <input
+                                type="text"
+                                id="apellidos"
+                                value={apellidos}
+                                onChange={(e) => setApellidos(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="contrasena">Contraseña:</label>
+                            <input
+                                type="password"
+                                id="contrasena"
+                                value={contrasena}
+                                onChange={(e) => setContrasena(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="nombre">Nombre(s):</label>
+                            <input
+                                type="text"
+                                id="nombre"
+                                value={nombre}
+                                onChange={(e) => setNombre(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="tipoUsuario">Puesto:</label>
+                            <select
+                                id="tipoUsuario"
+                                value={tipoUsuario}
+                                onChange={(e) => setTipoUsuario(e.target.value)}
+                                required>
+                                //opciones solo de ejemplo para probar
+                                <option value="">Seleccionar tipo</option>
+                                <option value="estudiante">Estudiante</option>
+                                <option value="profesor">Profesor</option>
+                                <option value="administrativo">Administrativo</option>
+                            </select>
+                    </div>
+                    <button type="submit" className="submit-button">Registrar</button>
+                 </form>
+            </div> 
         </div>
-    </div>
     );
 }
 
