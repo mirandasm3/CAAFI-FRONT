388335143b3133b1d2c1a892b111df3573f53149
mirandasm3 <130334715+mirandasm3@users.noreply.github.com>
2024-06-14 02:10:01 -0600

    a

--- src/pages/AcceptInscription.jsx
+++ src/pages/AcceptInscription.jsx
@@ -3,53 +3,52 @@ import { useNavigate } from "react-router-dom";
 import "../styles/accept-inscription.css";
 import Cookies from 'js-cookie';
 import UserIcon from "../components/UserIcon";
+import { Modal, Button } from 'react-bootstrap';
 
 export default function AcceptInscription() {
     const [students, setStudents] = useState([]);
-    const [selectedStudents, setSelectedStudents] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [selectedStudent, setSelectedStudent] = useState(null);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const [showSuccessModal, setShowSuccessModal] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
-        fetchStudents();
+        fetch('https://8kzxktht-3000.usw3.devtunnels.ms/inscripcion')
+            .then(response => response.json())
+            .then(data => setStudents(data))
+            .catch(error => console.error('Error fetching data:', error));
     }, []);
 
-    const fetchStudents = async () => {
-        try {
-            const response = await fetch('http://your-api-url/students');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            setStudents(data.students);
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-        }
-    };
+    const filteredStudents = students.filter(student =>
+        student.status.toLowerCase() === 'pendiente' &&
+        (student.nombre.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        student.apellidos.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        student.matricula.toLowerCase().includes(searchTerm.toLowerCase()))
+    );
 
     const handleSelectStudent = (matricula) => {
-        setSelectedStudents((prevSelected) => {
-            if (prevSelected.includes(matricula)) {
-                return prevSelected.filter((id) => id !== matricula);
-            } else {
-                return [...prevSelected, matricula];
-            }
-        });
+        if (selectedStudent === matricula) {
+            setSelectedStudent(null);
+        } else {
+            setSelectedStudent(matricula);
+        }
     };
 
     const handleApproveSelection = async () => {
         try {
-            const response = await fetch('http://your-api-url/approve-students', {
+            const response = await fetch('https://8kzxktht-3000.usw3.devtunnels.ms/inscripcion/accept', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${Cookies.get('auth')}`
                 },
-                body: JSON.stringify({ matriculas: selectedStudents })
+                body: JSON.stringify({ matricula: selectedStudent })
             });
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            fetchStudents();
+            setShowSuccessModal(true);
         } catch (error) {
             console.error('There was a problem with the fetch operation:', error);
         }
@@ -62,19 +61,21 @@ export default function AcceptInscription() {
                     <img src={require('../img/caafi-w.png')} alt="Logo CAAFI" />
                 </div>
                 <div className="m-login__head" style={{ position: 'absolute', top: '0', right: '10px', backgroundColor: '#0D47A1', zIndex: '1000', padding: '10px', borderBottomLeftRadius: '10px', borderBottomRightRadius: '10px' }}>
-                                <span className="g-font-weight-400 g-py-0 g-px-12 g-font-size-18" style={{ color: '#ffffff' }}>Universidad Veracruzana</span>
+                    <span className="g-font-weight-400 g-py-0 g-px-12 g-font-size-18" style={{ color: '#ffffff' }}>Universidad Veracruzana</span>
                 </div>
                 <UserIcon />
             </div>
-            <div className="back-button">
-                <button className="back-btn" onClick={() => navigate(-1)}>
-                    <i className="bi bi-arrow-left"></i>
-                </button>
-                <h2>Solicitudes de inscripción</h2>
-            </div>
-            <div className="actions">
-                <button className="manual-button">Inscripción manual</button>
-                <button className="approve-button" onClick={handleApproveSelection}>Aprobar selección</button>
+            <div className="header-bar">
+                <div className="back-button">
+                    <button className="back-btn" onClick={() => navigate(-1)}>
+                        <i className="bi bi-arrow-left"></i>
+                    </button>
+                    <h2>Solicitudes de inscripción</h2>
+                </div>
+                <div className="actions">
+                    <button className="manual-button">Inscripción manual</button>
+                    <button className="approve-button" onClick={() => setShowConfirmModal(true)} disabled={!selectedStudent}>Aprobar selección</button>
+                </div>
             </div>
             <table>
                 <thead>
@@ -89,18 +90,19 @@ export default function AcceptInscription() {
                     </tr>
                 </thead>
                 <tbody>
-                    {students.map((student) => (
+                    {filteredStudents.map((student) => (
                         <tr key={student.matricula}>
                             <td>{student.nombre}</td>
                             <td>{student.apellidos}</td>
-                            <td>{student.tipoAlumno}</td>
-                            <td>{student.tipoInscripcion}</td>
-                            <td>{student.comprobantePago}</td>
-                            <td>{student.bitacoraCero}</td>
+                            <td>{student.tipo}</td>
+                            <td>{student.inscripcion}</td>
+                            <td>{student.comprobante1 || 'N/A'}</td>
+                            <td>{student.comprobante2 || 'N/A'}</td>
                             <td>
                                 <input
-                                    type="checkbox"
-                                    checked={selectedStudents.includes(student.matricula)}
+                                    type="radio"
+                                    name="selectStudent"
+                                    checked={selectedStudent === student.matricula}
                                     onChange={() => handleSelectStudent(student.matricula)}
                                 />
                             </td>
@@ -108,6 +110,40 @@ export default function AcceptInscription() {
                     ))}
                 </tbody>
             </table>
+
+            <Modal show={showConfirmModal} onHide={() => setShowConfirmModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmar Aprobación</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    ¿Deseas confirmar la aprobación de inscripción de los alumnos seleccionados?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowConfirmModal(false)}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary" onClick={() => { setShowConfirmModal(false); handleApproveSelection(); }}>
+                        Confirmar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={showSuccessModal} onHide={() => setShowSuccessModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Registro Exitoso</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    El registro de alumnos ha sido guardado.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={() => setShowSuccessModal(false)}>
+                        Cerrar
+                    </Button>
+                    <button className="back-btn" onClick={() => navigate("/inicio")}>
+                        <i className="bi bi-arrow-left"></i>
+                    </button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
--- src/pages/StudentManagement.jsx
+++ src/pages/StudentManagement.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../styles/student-management.css';
+import { useNavigate } from "react-router-dom";
+import "../styles/student-management.css";
 import UserIcon from '../components/UserIcon';
+import { Modal, Button } from 'react-bootstrap';
 
 export default function StudentManagement() {
     const [students, setStudents] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [selectedStudent, setSelectedStudent] = useState(null);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const [showSuccessModal, setShowSuccessModal] = useState(false);
+    const [showStudentModal, setShowStudentModal] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -16,11 +21,47 @@ export default function StudentManagement() {
     }, []);
 
     const filteredStudents = students.filter(student =>
-        student.status.toLowerCase() === 'aprobado' &&
+        student.status === 'aprobado' &&
         (student.nombre.toLowerCase().includes(searchTerm.toLowerCase()) ||
         student.apellidos.toLowerCase().includes(searchTerm.toLowerCase()) ||
         student.matricula.toLowerCase().includes(searchTerm.toLowerCase()))
     );
+
+    const handleDeleteStudent = async () => {
+        if (!selectedStudent) return;
+
+        try {
+            const response = await fetch('https://8kzxktht-3000.usw3.devtunnels.ms/alumno', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ matricula: selectedStudent })
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            setShowSuccessModal(true);
+            setStudents(students.filter(student => student.matricula !== selectedStudent));
+            setSelectedStudent(null);
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    };
+
+    const handleViewStudent = async (matricula) => {
+        try {
+            const response = await fetch(`https://8kzxktht-3000.usw3.devtunnels.ms/alumno?matricula=${matricula}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setSelectedStudent(data);
+            setShowStudentModal(true);
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    };
 
     return (
         <div className="container-student-management">
@@ -33,17 +74,19 @@ export default function StudentManagement() {
                 </div>
                 <UserIcon />
             </div>
-            <div className="back-button">
-                <button className="back-btn" onClick={() => navigate(-1)}>&larr;</button>
-            </div>
-            <h2>Alumnos CAAFI</h2>
-            <div className="search-bar">
-                <input
-                    type="text"
-                    placeholder="Buscar..."
-                    value={searchTerm}
-                    onChange={e => setSearchTerm(e.target.value)}
-                />
+            <div className="header-bar">
+                <div className="back-button">
+                    <button className="back-btn" onClick={() => navigate(-1)}>&larr;</button>
+                </div>
+                <h2 className="header-title">Alumnos CAAFI</h2>
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        placeholder="Buscar..."
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                    />
+                </div>
             </div>
             <table>
                 <thead>
@@ -63,14 +106,66 @@ export default function StudentManagement() {
                             <td>{student.apellidos}</td>
                             <td>{student.tipo}</td>
                             <td>
-                                <button className="view-btn">Ver</button>
+                                <button className="view-btn" onClick={() => handleViewStudent(student.matricula)}>Ver</button>
                                 <button className="edit-btn">Editar</button>
-                                <button className="delete-btn">Eliminar</button>
+                                <button className="delete-btn" onClick={() => { setSelectedStudent(student.matricula); setShowConfirmModal(true); }}>Eliminar</button>
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+
+            <Modal show={showConfirmModal} onHide={() => setShowConfirmModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmar Eliminación</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Una vez confirmada la acción no se podrán deshacer los cambios, ¿está seguro de eliminar el registro del alumno?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowConfirmModal(false)}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary" onClick={() => { setShowConfirmModal(false); handleDeleteStudent(); }}>
+                        Confirmar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={showSuccessModal} onHide={() => setShowSuccessModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Registro Eliminado</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    El registro del alumno ha sido eliminado.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={() => setShowSuccessModal(false)}>
+                        Cerrar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={showStudentModal} onHide={() => setShowStudentModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Detalles del Alumno</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {selectedStudent && (
+                        <div>
+                            <p><strong>Matrícula:</strong> {selectedStudent.matricula}</p>
+                            <p><strong>Nombre:</strong> {selectedStudent.nombre}</p>
+                            <p><strong>Apellidos:</strong> {selectedStudent.apellidos}</p>
+
+                        </div>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={() => setShowStudentModal(false)}>
+                        Cerrar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
